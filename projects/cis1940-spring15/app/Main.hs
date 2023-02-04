cf1273506cb45035886e9fb7b74e1084ec6b6d46
rhirano0715 <hirano.ryuko.0715@gmail.com>
2023-02-04 23:11:36 +0000

    wip. add int minbound

--- projects/cis1940-spring15/app/Main.hs
+++ projects/cis1940-spring15/app/Main.hs
@@ -1,11 +1,13 @@
 module Main where
 
-import qualified Chap01 (intMax)
+import qualified Chap01 (intMax, intMin)
 
 -- |
 -- main
 -- >>> main
 -- 9223372036854775807
+-- -9223372036854775808
 main :: IO ()
 main = do
   print Chap01.intMax
+  print Chap01.intMin

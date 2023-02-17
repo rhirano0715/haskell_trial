ace93c7e67a79f94bfc6ec97e7b85bc49f94f269
rhirano0715 <hirano.ryuko.0715@gmail.com>
2023-02-17 13:08:43 +0000

    Modified to run from any dir

--- projects/cis1940-spring15/all_test.sh
+++ projects/cis1940-spring15/all_test.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+echo $0
+
+SELF_DIR=$(cd $(dirname $0);pwd)
+
 function main  {
+    local dir=$1
+    local dir_bk=$(pwd)
+
+    cd ${dir}
     cabal test
     local rc=${?}
+    cd ${dir_bk}
+
     echo "test end with ${rc}"
     return ${rc}
 }
 
-main
+main "${SELF_DIR}"
 exit ${?}
 

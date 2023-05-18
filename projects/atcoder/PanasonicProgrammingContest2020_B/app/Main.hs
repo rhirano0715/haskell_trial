cc15995a140ff408b064ff782ceb30d27fa6b097
rhirano0715 <hirano.ryuko.0715@gmail.com>
2023-05-18 13:14:07 +0000

    done.

--- projects/atcoder/PanasonicProgrammingContest2020_B/app/Main.hs
+++ projects/atcoder/PanasonicProgrammingContest2020_B/app/Main.hs
@@ -1,8 +1,5 @@
 module Main where
 
-import Control.Monad (replicateM)
-import Data.List (sort)
-
 -- |
 -- >>> solve 4 5
 -- 10
@@ -10,54 +7,29 @@ import Data.List (sort)
 -- 11
 -- >>> solve 1000000000 1000000000
 -- 500000000000000000
+-- >>> solve 1 1
+-- 1
+-- >>> solve 1 2
+-- 1
+-- >>> solve 1 3
+-- 1
+-- >>> solve 1 4
+-- 1
+-- >>> solve 1 5
+-- 1
+-- >>> solve 5 1
+-- 1
+-- >>> solve 4 1
+-- 1
 solve :: Int -> Int -> Int
-solve h w = solveStepH hs ws
+solve h w
+  | h == 1 = 1
+  | w == 1 = 1
+  | dived * 2 == hw = dived
+  | otherwise = dived + 1
   where
-    hs = makeArray h
-    ws = makeArray w
-    -- results = solveStepH hs ws
-    -- trues = filter (\r -> r == True) results
-
--- |
--- >>> makeArray 4
--- [1,2,3,4]
--- >>> makeArray 1
--- [1]
-makeArray :: Int -> [Int]
-makeArray max
-  | max <= 1 = [1]
-  | otherwise = [1 .. max]
-
-solveStepH :: [Int] -> [Int] -> Int
-solveStepH (h : hs) ws = (solveStepW h ws) + (solveStepH hs ws)
-solveStepH (h : []) ws = solveStepW h ws
-solveStepH [] ws = 0
-
--- |
--- >>> solveStepW 1 [1]
--- 1
--- >>> solveStepW 1 [1,2]
--- 1
--- >>> solveStepW 1 [1,3]
--- 2
-solveStepW :: Int -> [Int] -> Int
-solveStepW h (w : ws) = (couldMove h w) + solveStepW h ws
-solveStepW h (w : []) = (couldMove h w)
-solveStepW _ [] = 0
-
--- |
--- >>> couldMove 1 1
--- 1
--- >>> couldMove 1 3
--- 1
--- >>> couldMove 1 2
--- 0
-couldMove :: Int -> Int -> Int
-couldMove h w
-  | result == True = 1
-  | otherwise = 0
-  where
-    result = mod (h + w) 2 == 0
+    hw = h * w
+    dived = hw `div` 2
 
 main :: IO ()
 main = do

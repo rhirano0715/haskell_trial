25a00642558cdcb44fe5ffdd8795f5f7bc242df1
rhirano0715 <hirano.ryuko.0715@gmail.com>
2023-08-15 03:21:48 +0000

    mod

--- projects/atcoder/AtCoderBeginnerContest081_B/app/Main.hs
+++ projects/atcoder/AtCoderBeginnerContest081_B/app/Main.hs
@@ -3,31 +3,27 @@
 -- Description : https://atcoder.jp/contests/abc081/tasks/abc081_b
 module Main where
 
-import Data.Char
-import Numeric
+import Data.Bits
 
 -- |
--- >>> solve 3 [8,12,40]
+-- >>> solve [8,12,40]
 -- 2
 --
--- >>> solve 4 [5,6,8,10]
+-- >>> solve [5,6,8,10]
 -- 0
 --
--- >>> solve 6 [382253568, 723152896, 37802240, 379425024, 404894720, 471526144]
+-- >>> solve [382253568, 723152896, 37802240, 379425024, 404894720, 471526144]
 -- 8
-solve :: Int -> [Int] -> Int
-solve _ [] = 0
-solve _ as = minimum $ map solveOne as
+solve :: [Int] -> Int
+solve as = minimum $ map solveOne as
 
 solveOne :: Int -> Int
 solveOne a
-  | even a = 1 + solveOne dividedA
+  | even a = countTrailingZeros a
   | otherwise = 0
-  where
-    dividedA = a `div` 2
 
 main :: IO ()
 main = do
-  n <- readLn
+  _ <- getLine
   a <- fmap (map read . words) getLine
-  print $ solve n a
+  print $ solve a

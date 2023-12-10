009429b189c060f6184c21bb7edda28863afde79
rhirano0715 <104247605+rhirano0715@users.noreply.github.com>
2023-12-10 09:03:10 +0000

    Done.

--- projects/atcoder/AtCoderBeginnerContest141_C/app/Main.hs
+++ projects/atcoder/AtCoderBeginnerContest141_C/app/Main.hs
@@ -3,6 +3,7 @@
 -- Description : https://atcoder.jp/contests/abc141/tasks/abc141_c
 module Main where
 
+import qualified Data.IntMap as Map
 import Control.Monad (replicateM)
 
 -- |
@@ -15,22 +16,15 @@ import Control.Monad (replicateM)
 -- >>> solve 10 13 15 [3,1,4,1,5,9,2,6,5,3,5,8,9,7,9]
 -- ["No","No","No","No","Yes","No","No","No","Yes","No"]
 solve :: Int -> Int -> Int -> [Int] -> [String]
-solve n k q answers = [pass p | p <- lastPoints]
+solve n k q answers = map (result . (`Map.lookup` finalScores)) [1..n]
   where
-    startPoints = replicate n k
-    players = [0..n-1]
-    steps = [ [if x == a - 1 then 0 else -1 | x <- players] | a <- answers ]
-    lastPoints = foldl (zipWith (+)) startPoints steps
-
-
-pass :: Int -> String
-pass point
-  | point > 0 = "Yes"
-  | otherwise = "No"
+    initialScores = Map.fromList $ zip [1..n] (repeat (k - q))
+    finalScores = foldl (\acc x -> Map.adjust (+1) x acc) initialScores answers
+    result (Just score) | score > 0 = "Yes"
+    result _ = "No"
 
 main :: IO ()
 main = do
-  [n, k, q] <- map read . words <$> getLine
-  answers <- replicateM q (read <$> getLine)
-
-  mapM_ putStrLn $ solve n k q answers
+    [n, k, q] <- map read . words <$> getLine
+    answers <- replicateM q (read <$> getLine)
+    mapM_ putStrLn $ solve n k q answers

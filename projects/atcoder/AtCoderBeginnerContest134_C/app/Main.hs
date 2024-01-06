b1b6de8c3d6f97737f22b3d4172738a5bb7db56b
rhirano0715 <104247605+rhirano0715@users.noreply.github.com>
2024-01-06 06:36:48 +0000

    add: done

--- projects/atcoder/AtCoderBeginnerContest134_C/app/Main.hs
+++ projects/atcoder/AtCoderBeginnerContest134_C/app/Main.hs
@@ -3,7 +3,26 @@
 -- Description : https://atcoder.jp/contests/abc134/tasks/abc134_c
 module Main where
 
--- Main function to find the minimum time
+import Control.Monad
+import Data.List
+
 main :: IO ()
 main = do
-    print "Hello, world!"
+  n <- readLn :: IO Int
+  a <- replicateM n readLn :: IO [Int]
+  let results = solve a
+  mapM_ print results
+
+-- | Solve the problem
+--
+-- >>> solve [1, 4, 3]
+-- [4,3,4]
+--
+-- >>> solve [5, 5]
+-- [5,5]
+solve :: [Int] -> [Int]
+solve a = map replaceWithMax a
+  where
+    maxVal = maximum a
+    secondMaxVal = maximum $ delete maxVal a
+    replaceWithMax x = if x == maxVal then secondMaxVal else maxVal

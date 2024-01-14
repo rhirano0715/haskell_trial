c661d3b18572e95213f659282209a822dd7798bb
rhirano0715 <104247605+rhirano0715@users.noreply.github.com>
2024-01-14 05:09:40 +0000

    update: done

--- projects/atcoder/AtCoderBeginnerContest118_B/app/Main.hs
+++ projects/atcoder/AtCoderBeginnerContest118_B/app/Main.hs
@@ -3,6 +3,45 @@
 -- Description : https://atcoder.jp/contests/abc118/tasks/abc118_b
 module Main where
 
+import Control.Monad (replicateM)
+import Data.List (intersect)
+
+-- Main function
 main :: IO ()
 main = do
-  putStrLn "Hello, World!"
+  -- Read the number of people and the number of types of food
+  [n, _] <- map read . words <$> getLine
+  
+  -- Read the list of each person's favorite foods
+  favoriteFoodsLists <- replicateM n (map read . tail . words <$> getLine) :: IO [[Int]]
+
+  -- Output the result
+  print $ countFoodsLovedByEveryone favoriteFoodsLists
+
+-- | Function that takes a list of each person's favorite foods and returns the number of foods common to everyone
+--
+-- >>> countFoodsLovedByEveryone [[1,3],[1,2,3],[3,2]]
+-- 1
+--
+-- >>> countFoodsLovedByEveryone [[2,3,4,5],[1,3,4,5],[1,2,4,5],[1,2,3,5],[1,2,3,4]]
+-- 0
+--
+-- >>> countFoodsLovedByEveryone [[5,10,30]]
+-- 3
+countFoodsLovedByEveryone :: [[Int]] -> Int
+countFoodsLovedByEveryone favoriteFoodsLists = length $ foodsLovedByEveryone favoriteFoodsLists
+
+-- | Function that takes a list of each person's favorite foods and returns the foods common to everyone
+--
+-- >>> foodsLovedByEveryone [[1,3],[1,2,3],[3,2]]
+-- [3]
+--
+-- >>> foodsLovedByEveryone [[2,3,4,5],[1,3,4,5],[1,2,4,5],[1,2,3,5],[1,2,3,4]]
+-- []
+--
+-- >>> foodsLovedByEveryone [[5,10,30]]
+-- [5,10,30]
+foodsLovedByEveryone :: [[Int]] -> [Int]
+foodsLovedByEveryone [] = []
+foodsLovedByEveryone (firstPersonFoods:rest) = foldl intersect firstPersonFoods rest
+

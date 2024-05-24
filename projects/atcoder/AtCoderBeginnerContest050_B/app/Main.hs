d614c2b2b91fd0f423abea1904e6222f9c2064d9
rhirano0715 <104247605+rhirano0715@users.noreply.github.com>
2024-05-24 21:13:33 +0000

    update: Refactor input parsing.

--- projects/atcoder/AtCoderBeginnerContest050_B/app/Main.hs
+++ projects/atcoder/AtCoderBeginnerContest050_B/app/Main.hs
@@ -3,18 +3,33 @@
 -- Description : https://atcoder.jp/contests/abc050/tasks/abc050_b
 module Main where
 
-import Control.Monad (replicateM)
+import Control.Monad (mapM)
 
 main :: IO ()
 main = do
     n <- readLn :: IO Int
-    t <- map read . words <$> getLine :: IO [Int]
+    t <- map read . words <$> getLine
     m <- readLn :: IO Int
-    px <- replicateM m $ do
-        [p, x] <- map read . words <$> getLine
-        return (p, x)
+    px <- mapM (\_ -> (\[p, x] -> (p, x)) . map read . words <$> getLine) [1..m]
     let totalTime = sum t
-    mapM_ (print . updateTotalTime totalTime t) px
+    mapM_ (print . calculateNewTime totalTime t) px
 
-updateTotalTime :: Int -> [Int] -> (Int, Int) -> Int
-updateTotalTime totalTime t (p, x) = totalTime - t !! (p - 1) + x
+calculateNewTime :: Int -> [Int] -> (Int, Int) -> Int
+calculateNewTime totalTime t (p, x) = totalTime - t !! (p - 1) + x
+
+-- import Control.Monad (replicateM)
+
+-- main :: IO ()
+-- main = do
+--     n <- readLn :: IO Int
+--     t <- map read . words <$> getLine
+--     m <- readLn :: IO Int
+--     px <- replicateM m parseInput
+--     let totalTime = sum t
+--     mapM_ (print . calculateNewTime totalTime t) px
+
+-- parseInput :: IO (Int, Int)
+-- parseInput = (\[p, x] -> (p, x)) . map read . words <$> getLine
+
+-- calculateNewTime :: Int -> [Int] -> (Int, Int) -> Int
+-- calculateNewTime totalTime t (p, x) = totalTime - t !! (p - 1) + x
